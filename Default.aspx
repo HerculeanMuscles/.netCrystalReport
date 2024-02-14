000fcf8e866fa507f8e2fce998c948abd0c95e14
fahri <fahrisiswotokadelandenan@gmail.com>
2024-02-14 10:30:28 +0100

    added input functionality with debug lines

--- Default.aspx
+++ Default.aspx
@@ -7,15 +7,86 @@
 </head>
 <body>
     <form id="form1" runat="server">
+
+         <div id="formContainer ">
+             <div id="inputFormSection">
+                 <label for="txtSenderName">Sender Name:</label>
+                 <asp:TextBox ID="txtSenderName" runat="server"></asp:TextBox>
+
+                 <label for="txtSenderAddress">Sender Address:</label>
+                 <asp:TextBox ID="txtSenderAddress" runat="server"></asp:TextBox><br />
+
+                 <label for="txtSenderStateZip">SenderStateZip:</label>
+                 <asp:TextBox ID="txtSenderStateZip" runat="server"></asp:TextBox><br />
+
+                 <label for="txtEmail">Email:</label>
+                 <asp:TextBox ID="txtEmail" runat="server"></asp:TextBox><br />
+         
+                 <label for="txtDateToday">Date Today:</label>
+                 <asp:TextBox ID="txtDateToday" runat="server"></asp:TextBox><br />
+
+                 <label for="txtReceiverName">Receiver Name:</label>
+                 <asp:TextBox ID="txtReceiverName" runat="server"></asp:TextBox><br />
+
+                 <label for="txtTitle">Title:</label>
+                 <asp:TextBox ID="txtTitle" runat="server"></asp:TextBox><br />
+
+                 <label for="txtOrganizationName">Organization Name:</label>
+                 <asp:TextBox ID="txtOrganizationName" runat="server"></asp:TextBox><br />
+
+                 <label for="txtReceiverAddress">Receiver Address:</label>
+                 <asp:TextBox ID="txtReceiverAddress" runat="server"></asp:TextBox><br />
+
+                 <label for="txtReceiverStateZip">ReceiverStateZip:</label>
+                 <asp:TextBox ID="txtReceiverStateZip" runat="server"></asp:TextBox><br />
+
+                 <label for="txtLastEmploymentDate">LastEmploymentDate:</label>
+                 <asp:TextBox ID="txtLastEmploymentDate" runat="server"></asp:TextBox><br />
+
+                 <label for="txtAidDetails">AidDetails:</label>
+                 <asp:TextBox ID="txtAidDetails" runat="server"></asp:TextBox><br />
+
+                 <asp:Button ID="btnGenerateReport" runat="server" Text="Generate Report" OnClick="btnGenerateReport_Click" />
+                 <asp:Button ID="btnAdd2Excel" runat="server" Text="Add to Excel" OnClick="btnAdd2Excel_Click" />
+                 <br />
+
+                 <div id="FileUploadSection">
+                     <asp:FileUpload ID="FileUpload1" runat="server" AllowMultiple="True" />
+                        <br />
+
+                     <asp:Button ID="Button1" runat="server" Text="Submit" OnClick="test" />
+                     <br />
+                 </div>
+
+             </div>
+        </div>
+
+
+
+
+
     <div>
         <CR:CrystalReportViewer ID="CrystalReportViewer1" runat="server" AutoDataBind="True"
             Height="1039px" ReportSourceID="CrystalReportSource1" Width="901px" />
         <CR:CrystalReportSource ID="CrystalReportSource1" runat="server">
-            <Report FileName="LayoffReport.rpt">
+            <Report FileName="CrystalReport1.rpt">
             </Report>
         </CR:CrystalReportSource>
     
     </div>
     </form>
+
+        <!-- jQuery and jQuery UI scripts -->
+    <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
+    <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css" />
+    <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+
+    <!-- Datepicker script -->
+    <script>
+        $(function () {
+            $("#<%=txtLastEmploymentDate.ClientID%>").datepicker();
+            $("#<%=txtDateToday.ClientID%>").datepicker();
+        });
+    </script>
 </body>
 </html>
--- Default.aspx.cs
+++ Default.aspx.cs
@@ -14,13 +14,14 @@ using CrystalDecisions.Shared;
 using CrystalDecisions.CrystalReports.Engine;
 using CrystalDecisions.Web;
 using System.Data.OleDb;
+using System.Diagnostics;
 
 public partial class _Default : System.Web.UI.Page 
 {
 
     private const string ExcelFilePath = "C:\\Users\\Fahri\\source\\repos\\Wazaran1\\Contracts2\\LayoffContract.xlsx";
     private const string ExcelConnectionString = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" + ExcelFilePath + ";Extended Properties=\"Excel 12.0 Xml;HDR=YES\";";
-    private const string crystalreportFile1 = "LayoffReport.rpt";
+    private const string LayoffReport = "LayoffReport.rpt"; private const string OfferReport = "OfferReport.rpt";
     ReportDocument report = new ReportDocument();
 
     protected void Page_Init(object sender, EventArgs e)
@@ -45,7 +46,7 @@ public partial class _Default : System.Web.UI.Page
     {
         try
         {
-            report.Load(Server.MapPath(crystalreportFile1));
+            report.Load(Server.MapPath(LayoffReport));
 
             DataTable testData = GetTestDataFromExcel(); // retrieve data here
 
@@ -79,4 +80,82 @@ public partial class _Default : System.Web.UI.Page
             }
         }
     }
-}
+
+    protected void btnGenerateReport_Click(object sender, EventArgs e)
+    {
+        // Create a DataSet
+        DataSet dataSet = new DataSet();
+
+        // Create a DataTable within the DataSet
+        DataTable dataTable = new DataTable("Layoff");
+
+        // Define columns in the DataTable (replace with your actual column names and types)
+        dataTable.Columns.Add("SenderName", typeof(string));
+        dataTable.Columns.Add("SenderAddress", typeof(string));
+        dataTable.Columns.Add("SenderStateZip", typeof(string));
+        dataTable.Columns.Add("txtEmail", typeof(string));
+        dataTable.Columns.Add("DateToday", typeof(DateTime));
+        dataTable.Columns.Add("LastEmploymentDate", typeof(DateTime));
+        dataTable.Columns.Add("ReceiverName", typeof(string));
+        dataTable.Columns.Add("Title", typeof(string));
+        dataTable.Columns.Add("OrganizationName", typeof(string));
+        dataTable.Columns.Add("ReceiverAddress", typeof(string));
+        dataTable.Columns.Add("ReceiverStateZip", typeof(string));
+        dataTable.Columns.Add("AidDetails", typeof(string));
+        // Create a new row and fill it with form data
+        DataRow dataRow = dataTable.NewRow();
+        dataRow["SenderName"] = txtSenderName.Text;
+        dataRow["SenderAddress"] = txtSenderAddress.Text;
+        dataRow["SenderStateZip"] = txtSenderStateZip.Text;
+        dataRow["txtEmail"] = txtEmail.Text;
+        dataRow["DateToday"] = DateTime.Parse(txtDateToday.Text).Date; // Parse the date from the TextBox and remove the time
+        dataRow["ReceiverName"] = txtReceiverName.Text;
+        dataRow["Title"] = txtTitle.Text;
+        dataRow["LastEmploymentDate"] = DateTime.Parse(txtLastEmploymentDate.Text).Date; // Parse the date from the TextBox and remove the time
+        dataRow["OrganizationName"] = txtOrganizationName.Text;
+        dataRow["ReceiverAddress"] = txtReceiverAddress.Text;
+        dataRow["ReceiverStateZip"] = txtReceiverStateZip.Text;
+        dataRow["AidDetails"] = txtAidDetails.Text;
+
+        // Add the row to the DataTable
+        dataTable.Rows.Add(dataRow);
+
+        // Log DataTable contents to the console or other logging mechanism
+        LogDataTableContents(dataTable);
+
+        // Add the DataTable to the DataSet
+        dataSet.Tables.Add(dataTable);
+
+        // For example, bind the data to a Crystal Report Viewer
+        report.Load(Server.MapPath(LayoffReport));
+        report.SetDataSource(dataTable);
+        CrystalReportViewer1.ReportSource = report;
+        Session["ReportDocument"] = report;
+
+        // Set the Crystal Report as the report source for the CrystalReportViewer
+        CrystalReportViewer1.ReportSource = report;
+    }
+
+    // Helper method to log DataTable contents
+    private void LogDataTableContents(DataTable dataTable)
+    {
+        foreach (DataRow row in dataTable.Rows)
+        {
+            foreach (DataColumn col in dataTable.Columns)
+            {
+                string columnName = col.ColumnName;
+                object value = row[col];
+                // Log or print the column name and value
+                Debug.WriteLine(string.Format("{0}: {1}", columnName, value));
+
+            }
+        }
+    }
+
+    protected void btnAdd2Excel_Click(object sender, EventArgs e)
+    { }
+
+    protected void test(object sender, EventArgs e)
+    { }
+
+    }
